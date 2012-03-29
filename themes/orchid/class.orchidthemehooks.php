270aa811b6920581ca99299c6f0d1b6c5a221e34
chuck911 <chuck911@qq.com>
2012-03-29 20:45:14 +0800

    update orchid theme

--- themes/orchid/class.orchidthemehooks.php
+++ themes/orchid/class.orchidthemehooks.php
@@ -4,7 +4,7 @@ class OrchidThemeHooks extends Gdn_Plugin {
 	public function DiscussionsController_BeforeDiscussionContent_Handler($Sender){
 			$First = UserBuilder($Sender->EventArguments['Discussion'], 'First');
 			$photo = $First->Photo;
-			if(!$photo) $photoURL = Url('/themes/v2ex/design/default-avatar.jpg');
+			if(!$photo) $photoURL = Url('/themes/orchid/design/default-avatar.jpg');
 			else $photoURL = $photo;
 			echo '<a class="Author" href="'.Url('/profile/'.$First->UserID.'/'.urlencode($First->Name)).'">'
 				.'<img class="ProfilePhotoMedium" src="'.$photoURL.'" />'.'</a>';
