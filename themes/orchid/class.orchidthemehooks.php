d6911bfdbdeee324725fa03506e1a12e898b0d3e
chuck911 <chuck911@qq.com>
2012-07-22 16:18:18 +0800

    use IndexPhotos plugin instead

--- themes/orchid/class.orchidthemehooks.php
+++ themes/orchid/class.orchidthemehooks.php
@@ -1,15 +1,6 @@
 <?php if (!defined('APPLICATION')) exit();
 
-class OrchidThemeHooks extends Gdn_Plugin {
-	public function DiscussionsController_BeforeDiscussionContent_Handler($Sender){
-			$First = UserBuilder($Sender->EventArguments['Discussion'], 'First');
-			$photo = $First->Photo;
-			if(!$photo) $photoURL = Url('/themes/orchid/design/default-avatar.jpg');
-			else $photoURL = $photo;
-			echo '<a class="Author" href="'.Url('/profile/'.$First->UserID.'/'.urlencode($First->Name)).'">'
-				.'<img class="ProfilePhotoMedium" src="'.$photoURL.'" />'.'</a>';
-	}
-	
+class OrchidThemeHooks extends Gdn_Plugin {	
 	public function Base_AfterJsCdns_Handler($Sender,$Args){
 		$Args['Cdns'] = array('jquery.js' => 'http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.2.min.js');
 	}
