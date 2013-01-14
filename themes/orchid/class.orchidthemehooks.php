4ff2bd204745e130cedaaa30efe55d5ef42dd52c
chuck911 <chuck911@qq.com>
2013-01-14 13:07:08 +0800

    use sae cdn for jQuery

--- themes/orchid/class.orchidthemehooks.php
+++ themes/orchid/class.orchidthemehooks.php
@@ -2,7 +2,7 @@
 
 class OrchidThemeHooks extends Gdn_Plugin {	
 	public function Base_AfterJsCdns_Handler($Sender,$Args){
-		$Args['Cdns'] = array('jquery.js' => 'http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.2.min.js');
+		$Args['Cdns'] = array('jquery.js' => 'http://lib.sinaapp.com/js/jquery/1.6.2/jquery.min.js');
 	}
 }
 
