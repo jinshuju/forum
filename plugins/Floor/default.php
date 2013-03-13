5e9f41ca838768291a88ba9081c1c4c9672741ff
chuck911 <chuck911@qq.com>
2013-03-13 18:24:27 +0800

    add Floor plugin

---
+++ plugins/Floor/default.php
@@ -0,0 +1,48 @@
+<?php if (!defined('APPLICATION')) exit();
+
+$PluginInfo['Floor'] = array(
+	'Name' => 'Floor 盖楼',
+	'Description' => 'Show floor number in discussion 在帖子种显示楼层号',
+	'Version' => '1.0',
+	'RequiredApplications' => array('Vanilla' => '2.0.18.4'),
+	'RequiredTheme' => FALSE,
+	'RequiredPlugins' => FALSE,
+	'MobileFriendly' => TRUE,
+	// 'SettingsUrl' => '/dashboard/settings/baidulike',
+	'SettingsPermission' => 'Garden.Settings.Manage',
+	'HasLocale' => TRUE,
+	'RegisterPermissions' => FALSE,
+	'Author' => "chuck911",
+	'AuthorEmail' => 'contact@with.cat',
+	'AuthorUrl' => 'http://vanillaforums.cn/profile/chuck911'
+);
+
+class FloorPlugin extends Gdn_Plugin {
+	public $number = 1;
+
+	public function DiscussionController_DiscussionInfo_Handler($Sender) {
+		if(C('Plugins.Floor.FirstFloor',true))
+			$this->showFloorNumber();
+		else
+			$this->number++;
+	}
+
+	public function DiscussionController_CommentInfo_Handler($Sender,$Args) {
+		$comment = $Args['Object'];
+		$this->showFloorNumber($comment->CommentID);
+	}
+
+	protected function showFloorNumber($fragment = null)
+	{
+		echo ' <span class="MItem Floor">';
+		if($fragment)
+			echo '<a href="#Comment_'.$fragment.'">'.T('#'.$this->number++).'</a>';
+		else
+			echo T('#'.$this->number++);
+		echo '</span>';
+	}
+
+	public function DiscussionController_Render_Before(&$Sender) {
+		$Sender->AddCssFile('floor.css', 'plugins/Floor');
+	}
+}
---
+++ plugins/Floor/locale/zh-CN.php
@@ -0,0 +1,4 @@
+<?php if (!defined('APPLICATION')) exit();
+$Definition['#1'] = '楼主';
+$Definition['#2'] = '沙发';
+$Definition['#3'] = '板凳';
