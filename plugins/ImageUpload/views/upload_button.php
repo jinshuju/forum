6e69b36af19f1e5beaf28b2e327aa2144626f44c
chuck911 <chuck911@qq.com>
2013-08-06 00:37:14 +0800

    ImageUpload plugin 图片上传插件

---
+++ plugins/ImageUpload/views/upload_button.php
@@ -0,0 +1,4 @@
+<div class="ImageUploadWrapper">
+<button type="button" id="btn_imageupload"><img class="icon_imageupload" src="/plugins/ImageUpload/img/imageupload.png"></button>
+<img style="display:none;" id="imageupload_loading" src="/plugins/ImageUpload/img/ajax-loader.gif">
+</div>
